704aea5985db714067f5bb6792a530fd89ab4fa9
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-04-02 18:05:24 +0300

    Add files via upload
    прикрутил классы и таймер работы

--- IBubbleSort/Program.cs
+++ IBubbleSort/Program.cs
@@ -4,30 +4,34 @@ namespace IBubbleSort
 {
     class Program
     {
-        static void Swap(int i, int j)
+        public class Data
         {
-            int temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-            
-        }
-        static int[] a = new int[10] { 3, 0, 1, 8, 7, 2, 5, 4, 6, 9 };
+            public void Swap(int i, int j)
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
 
+            }
+            public int[] a = new int[10] { 3, 0, 1, 8, 7, 2, 5, 4, 6, 9 };
+        }
 
         static void Main(string[] args)
         {
+            Data Ex = new Data();
+
             DateTime DT = DateTime.Now;
             int swap = 0;
             //выведем на экран исходный массив
-            for (int i = 0; i < a.Length; i++)
-                Console.Write(a[i]+", ");
+            for (int i = 0; i < Ex.a.Length; i++)
+                Console.Write(Ex.a[i]+", ");
             //сортировка пузырьком
-                for (int i = 0; i < a.Length-1; i++)
+                for (int i = 0; i < Ex.a.Length-1; i++)
                 {
-                    for (int j = i+1; j < a.Length; j++)
+                    for (int j = i+1; j < Ex.a.Length; j++)
                     {
-                        if (a[i] > a[j])
-                        Swap(i, j);
+                        if (Ex.a[i] > Ex.a[j])
+                        Ex.Swap(i, j);
                         swap++ ;      
                     }  
                     if (swap == 0)
@@ -36,8 +40,8 @@ namespace IBubbleSort
                 //выводим результат массив
             Console.WriteLine();
 
-            for (int i = 0; i < a.Length; i++)
-                Console.Write(a[i] + ", ");
+            for (int i = 0; i < Ex.a.Length; i++)
+                Console.Write(Ex.a[i] + ", ");
 
             //Dim sw = new Stopwatch();
             //sw.Start();
